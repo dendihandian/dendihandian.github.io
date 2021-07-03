7f1e478ea16d9756d870a69f11f3da07ba1f1a49
Dendi Handian <handian.dendi@gmail.com>
2021-07-03 18:11:05 +0700

    apply alpinejs

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
-// Import all plugins
+// import data
+// import * as data from './data';
+
+// import bootstrap's all plugins
 import * as bootstrap from 'bootstrap';
+
+// import alpinejs
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
+// document ready event
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => { 
+  Alpine.start();
+  // alert('lorem');
+});
+
