e13d91672d69bdc5b65ae703206266a36f7e9e25
Dendi Handian <contact.dendi@yahoo.com>
2021-12-23 21:34:25 +0700

    experimenting for deployment (1)

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/dendihandian.github.io' : '',
+  'process.env.BACKEND_URL': prod ? 'https://dendihandian.github.io' : '',
 }
