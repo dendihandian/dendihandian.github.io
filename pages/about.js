c38bad5a63a7674fd48dfab07ee17f2e6d9b6676
Dendi Handian <contact.dendi@yahoo.com>
2021-12-23 22:24:38 +0700

    test meta tags

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,11 @@ import Link from 'next/link'
 export default function About() {
   return (
     <div>
+      <Head>
+        <title>Dendi Handian | About</title>
+        <meta name="description" content="About Dendi Handian" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div>About us</div>
       <div>
         Back to{' '}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div>
-      Hello World.{' '}
+      <Head>
+        <title>Dendi Handian</title>
+        <meta name="description" content="The official personal site of Dendi Handian" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
+        <a>Go to About</a>
       </Link>
     </div>
   )
