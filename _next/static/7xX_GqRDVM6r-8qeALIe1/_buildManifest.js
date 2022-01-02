3229679ec94f8bad0b225e2cd6bf0cd5610858c2
dendihandian <handian.dendi@gmail.com>
2022-01-02 22:18:07 +0000

    Deploying to gh-pages from  @ 73e0ae4e3209830983f8e9323640dc25ed3ecdf8 🚀

--- _next/static/7xX_GqRDVM6r-8qeALIe1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-efaacb7a9173b2f2.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],"/about":["static/chunks/pages/about-d0743e993f51f3a7.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
